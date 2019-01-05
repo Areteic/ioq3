618cc3f1a5eb06f7cc99dfea7462875057c48c29
SalamatiQus <SalamatiQus@users.noreply.github.com>
2019-01-05 12:13:52 +0600

    Update cg_view.c

--- code/cgame/cg_view.c
+++ code/cgame/cg_view.c
@@ -554,7 +554,7 @@ static int CG_CalcFov( void ) {
 	if ( !cg.zoomed ) {
 		cg.zoomSensitivity = 1;
 	} else {
-		cg.zoomSensitivity = tan( cg.refdef.fov_x * M_PI / 180 / 2) / tan( cg_fov.value * M_PI / 180 / 2);;
+		cg.zoomSensitivity = tan( cg.refdef.fov_x * M_PI / 180 / 2) / tan( cg_fov.value * M_PI / 180 / 2);
 	}
 
 	return inwater;
